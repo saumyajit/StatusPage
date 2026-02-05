ab485d168ddf8203fcc2f3dae31e2bc35e0d0e09
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 17:02:33 +0530

    Update statuspage.view.php

--- views/statuspage.view.php
+++ views/statuspage.view.php
@@ -1,41 +1,152 @@
-<?php declare(strict_types = 1);
-
+<?php
 /**
+ * Status Page View
  * @var CView $this
  * @var array $data
  */
 
-$this->addJsFile('class.statuspage.js');
-$this->includeJsFile('statuspage.view.js.php');
+// Include JS and CSS
+$this->includeJsFile('statuspage.js');
+$this->addCssFile('statuspage.css');
 
-$this->enableLayoutModes();
-$web_layout_mode = $this->getLayoutMode();
-
-$html_page = (new CHtmlPage())
-    ->setTitle(_('Status Page'))
-    ->setWebLayoutMode($web_layout_mode)
+// Create page widget
+$widget = (new CWidget())
+    ->setTitle($data['title'])
     ->setControls(
-        (new CTag('nav', true,
-            (new CList())
-                ->addItem(
-                    (new CSimpleButton(_('Settings')))
-                        ->onClick('statusPageSettings()')
-                        ->addClass(ZBX_STYLE_BTN_ALT)
-                )
+        (new CTag('nav', true, 
+            (new CButton('refresh', _('Refresh')))
+                ->onClick('StatusPage.refresh();')
         ))->setAttribute('aria-label', _('Content controls'))
     );
 
-// Main content widget
-$html_page->addItem(
-    (new CDiv([
-        (new CDiv())
-            ->setId('statuspage-controls')
-            ->addClass('statuspage-controls'),
-        (new CDiv())
-            ->setId('statuspage-container')
-            ->addClass('statuspage-container')
-    ]))
-        ->addClass('statuspage-wrapper')
-);
+// Filter form
+$filter_form = (new CForm('get'))
+    ->setId('status-page-filter')
+    ->setName('status_page_filter')
+    ->addVar('action', 'status.page');
 
-$html_page->show();
+$filter_table = (new CFormGrid())
+    ->addItem([
+        new CLabel(_('Items'), 'limit'),
+        new CFormField(
+            (new CSelect('limit'))
+                ->setId('limit')
+                ->setValue($data['filter']['limit'])
+                ->addOptions(CSelect::createOptionsFromArray($data['limit_options']))
+        )
+    ])
+    ->addItem([
+        new CLabel(_('Icon Size'), 'icon_size'),
+        new CFormField(
+            (new CSelect('icon_size'))
+                ->setId('icon_size')
+                ->setValue($data['filter']['icon_size'])
+                ->addOptions(CSelect::createOptionsFromArray($data['icon_sizes']))
+        )
+    ])
+    ->addItem([
+        new CLabel(_('Spacing'), 'spacing'),
+        new CFormField(
+            (new CSelect('spacing'))
+                ->setId('spacing')
+                ->setValue($data['filter']['spacing'])
+                ->addOptions(CSelect::createOptionsFromArray($data['spacing_options']))
+        )
+    ])
+    ->addItem([
+        new CLabel(_('Filter Groups'), 'filter_groups'),
+        new CFormField(
+            (new CTextBox('filter_groups', $data['filter']['groups']))
+                ->setId('filter_groups')
+                ->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
+                ->setAttribute('placeholder', _('Search hostgroup name...'))
+        )
+    ])
+    ->addItem([
+        new CLabel(_('Show Alerts Only'), 'filter_alerts_only'),
+        new CFormField(
+            (new CCheckBox('filter_alerts_only', 1))
+                ->setId('filter_alerts_only')
+                ->setChecked($data['filter']['alerts_only'] == 1)
+        )
+    ])
+    ->addItem([
+        new CFormField(
+            (new CSimpleButton(_('Apply')))
+                ->onClick('StatusPage.applyFilter();')
+                ->addClass(ZBX_STYLE_BTN_ALT)
+        )
+    ]);
+
+$filter_form->addItem($filter_table);
+
+// Add filter to widget
+$widget->addItem($filter_form);
+
+// Status summary section
+$summary_div = (new CDiv())
+    ->setId('status-summary')
+    ->addClass('status-summary')
+    ->addItem([
+        (new CDiv())
+            ->setId('total-groups')
+            ->addClass('summary-item')
+            ->addItem([
+                (new CSpan())->setId('total-groups-count')->addClass('summary-count'),
+                (new CSpan(_('HOST GROUPS')))->addClass('summary-label')
+            ]),
+        (new CDiv())
+            ->setId('healthy-groups')
+            ->addClass('summary-item')
+            ->addItem([
+                (new CSpan())->setId('healthy-groups-count')->addClass('summary-count'),
+                (new CSpan(_('HEALTHY')))->addClass('summary-label')
+            ]),
+        (new CDiv())
+            ->setId('alert-groups')
+            ->addClass('summary-item')
+            ->addItem([
+                (new CSpan())->setId('alert-groups-count')->addClass('summary-count'),
+                (new CSpan(_('WITH ALERTS')))->addClass('summary-label')
+            ])
+    ]);
+
+$widget->addItem($summary_div);
+
+// Canvas container
+$canvas_container = (new CDiv())
+    ->setId('status-canvas-container')
+    ->addClass('status-canvas-container')
+    ->addItem(
+        (new CTag('canvas', false))
+            ->setId('status-canvas')
+            ->setAttribute('width', '100%')
+            ->setAttribute('height', '600')
+    );
+
+$widget->addItem($canvas_container);
+
+// Tooltip container
+$tooltip = (new CDiv())
+    ->setId('status-tooltip')
+    ->addClass('status-tooltip')
+    ->addStyle('display: none;');
+
+$widget->addItem($tooltip);
+
+// Loading overlay
+$loading = (new CDiv(_('Loading...')))
+    ->setId('status-loading')
+    ->addClass('status-loading')
+    ->addStyle('display: none;');
+
+$widget->addItem($loading);
+
+// Initialize JavaScript
+$this->addJavaScript('
+    jQuery(document).ready(function() {
+        StatusPage.init(' . json_encode($data['filter']) . ');
+    });
+');
+
+$widget->show();
