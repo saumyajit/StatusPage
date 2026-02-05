5d31501a98e38629c0516f80215e70c3d0e622cb
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 19:38:00 +0530

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,24 +1,20 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace Modules\StatusPage;
 
-use Zabbix\Core\CModule;
+use Core\CModule;
 use APP;
 use CMenuItem;
 
 class Module extends CModule {
     
     public function init(): void {
-        // Register CSS file
-        APP::Component()->get('stylesheet')->addExternalStyle('modules/StatusPage/assets/css/statuspage.css');
-        
         // Add menu item
-        $menu = APP::Component()->get('menu.main')
+        APP::Component()->get('menu.main')
             ->findOrAdd(_('Monitoring'))
-            ->getSubmenu();
-            
-        $menu->insertAfter(_('Hosts'),
-            (new CMenuItem(_('Status Page')))->setAction('status.page')
-        );
+            ->getSubmenu()
+            ->insertAfter(_('Hosts'),
+                (new CMenuItem(_('Status Page')))->setAction('status.page')
+            );
     }
 }
