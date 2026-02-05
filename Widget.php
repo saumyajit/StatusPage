73b9ad48c8909f3c363beaa52bc7b422741325e3
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 10:47:10 +0530

    Update Widget.php

--- Widget.php
+++ Widget.php
@@ -6,24 +6,9 @@ use Zabbix\Core\CWidget;
 
 class Widget extends CWidget {
 
-    public const DEFAULT_REFRESH = 30;
-    public const ICON_SIZE_TINY = 20;
-    public const ICON_SIZE_SMALL = 30;
-    public const ICON_SIZE_MEDIUM = 40;
-    public const ICON_SIZE_LARGE = 50;
-
+    public const DEFAULT_NAME = 'Status Page';
+    
     public function getDefaultName(): string {
-        return _('Status Page');
-    }
-
-    public function getTranslationStrings(): array {
-        return [
-            'class.widget.js' => [
-                'No data' => _('No data'),
-                'Active alerts' => _('Active alerts'),
-                'No active alerts' => _('No active alerts'),
-                'Host Group' => _('Host Group')
-            ]
-        ];
+        return self::DEFAULT_NAME;
     }
 }
