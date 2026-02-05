1e70e0bbc9785fd76ed47515ac017e286686d308
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 10:47:58 +0530

    Update WidgetForm.php

--- WidgetForm.php
+++ WidgetForm.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types = 1);
 
-namespace Modules\StatusPageCompact\Actions;
+namespace Modules\StatusPageCompact;
 
-use Zabbix\Modules\CWidgetForm;
-use Zabbix\Modules\Fields\CWidgetFieldCheckBox;
-use Zabbix\Modules\Fields\CWidgetFieldMultiSelectGroup;
-use Zabbix\Modules\Fields\CWidgetFieldRadioButtonList;
+use Zabbix\Widgets\CWidgetForm;
+use Zabbix\Widgets\Fields\CWidgetFieldCheckBox;
+use Zabbix\Widgets\Fields\CWidgetFieldMultiSelectGroup;
+use Zabbix\Widgets\Fields\CWidgetFieldRadioButtonList;
 
 class WidgetForm extends CWidgetForm {
 
@@ -15,19 +15,21 @@ class WidgetForm extends CWidgetForm {
                 new CWidgetFieldMultiSelectGroup('groupids', _('Host groups'))
             )
             ->addField(
-                new CWidgetFieldRadioButtonList('icon_size', _('Icon Size'), 30, [
-                    20 => _('Tiny (20px)'),
-                    30 => _('Small (30px)'),
-                    40 => _('Medium (40px)'),
-                    50 => _('Large (50px)')
-                ])
+                (new CWidgetFieldRadioButtonList('icon_size', _('Icon Size'), 30))
+                    ->setValues([
+                        20 => _('Tiny (20px)'),
+                        30 => _('Small (30px)'),
+                        40 => _('Medium (40px)'),
+                        50 => _('Large (50px)')
+                    ])
             )
             ->addField(
-                new CWidgetFieldRadioButtonList('view_style', _('View Style'), 0, [
-                    0 => _('Honeycomb'),
-                    1 => _('Circle Grid'),
-                    2 => _('Square Grid')
-                ])
+                (new CWidgetFieldRadioButtonList('view_style', _('View Style'), 0))
+                    ->setValues([
+                        0 => _('Honeycomb'),
+                        1 => _('Circle Grid'),
+                        2 => _('Square Grid')
+                    ])
             )
             ->addField(
                 new CWidgetFieldCheckBox('show_alert_count', _('Show alert count badge'), 1)
