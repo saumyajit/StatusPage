a6d7406bfa371aff4252cac8ea6d4aaabb4eff53
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 19:34:52 +0530

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Modules\StatusPage;
 
 use Zabbix\Core\CModule;
@@ -6,14 +7,18 @@ use APP;
 use CMenuItem;
 
 class Module extends CModule {
+    
     public function init(): void {
-        $menu = APP::Component()->get('menu.main')
-            ->findOrAdd(_('Reports'))
-                ->getSubmenu();
+        // Register CSS file
+        APP::Component()->get('stylesheet')->addExternalStyle('modules/StatusPage/assets/css/statuspage.css');
         
-        // Add new Storage Analytics menu item
-        $menu->insertAfter(_('Notification'),
+        // Add menu item
+        $menu = APP::Component()->get('menu.main')
+            ->findOrAdd(_('Monitoring'))
+            ->getSubmenu();
+            
+        $menu->insertAfter(_('Hosts'),
             (new CMenuItem(_('Status Page')))->setAction('status.page')
-        );        
+        );
     }
 }
