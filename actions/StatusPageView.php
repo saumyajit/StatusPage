ccc0a645980eed2ada027c936eaa3e089d30d465
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 23:14:19 +0530

    Update StatusPageView.php

--- actions/StatusPageView.php
+++ actions/StatusPageView.php
@@ -16,8 +16,10 @@ class StatusPageView extends CController {
 
     protected function checkInput(): bool {
         $fields = [
-            'icon_size' => 'in tiny,small,medium,large',
+            'icon_size' => 'in 20,25,30,35,40',
             'spacing' => 'in normal,compact,ultra-compact',
+            'filter_alerts' => 'in 0,1',
+            'search' => 'string',
             'refresh' => 'in 0,1'
         ];
 
@@ -36,143 +38,196 @@ class StatusPageView extends CController {
 
     protected function doAction(): void {
         // Get user preferences
-        $icon_size = $this->getInput('icon_size', 'small');
+        $icon_size = $this->getInput('icon_size', '30');
         $spacing = $this->getInput('spacing', 'normal');
+        $filter_alerts = $this->hasInput('filter_alerts') ? (bool)$this->getInput('filter_alerts') : false;
+        $search = $this->getInput('search', '');
         $refresh = $this->hasInput('refresh') ? (bool)$this->getInput('refresh') : false;
 
         try {
-            // 1. Fetch host groups
-            $host_groups = API::HostGroup()->get([
+            // 1. Fetch host groups starting with "CUSTOMER/"
+            $all_host_groups = API::HostGroup()->get([
                 'output' => ['groupid', 'name'],
                 'preservekeys' => true
             ]);
 
-            // 2. Fetch hosts with their groups
+            // Filter groups that start with "CUSTOMER/"
+            $customer_groups = [];
+            foreach ($all_host_groups as $groupid => $group) {
+                if (strpos($group['name'], 'CUSTOMER/') === 0) {
+                    $customer_groups[$groupid] = $group;
+                }
+            }
+
+            // 2. Fetch hosts for these groups
             $hosts = API::Host()->get([
                 'output' => ['hostid', 'host', 'name', 'status'],
+                'groupids' => array_keys($customer_groups),
                 'selectHostGroups' => ['groupid'],
                 'monitored_hosts' => true,
                 'preservekeys' => true
             ]);
 
-            // 3. Fetch active triggers
+            // 3. Fetch active triggers with full details
             $triggers = API::Trigger()->get([
                 'output' => ['triggerid', 'description', 'priority', 'value'],
                 'selectHosts' => ['hostid'],
                 'filter' => ['value' => TRIGGER_VALUE_TRUE],
+                'monitored' => true,
                 'preservekeys' => true
             ]);
 
-            // Prepare statistics
-            $statistics = [
-                'total_groups' => count($host_groups),
-                'total_hosts' => count($hosts),
-                'healthy_hosts' => 0,
-                'hosts_with_alerts' => 0,
-                'critical_alerts' => 0,
-                'warning_alerts' => 0,
-                'info_alerts' => 0
-            ];
-
-            // Process each host
-            $host_status = [];
-            
-            // First, map triggers to hosts
-            $host_triggers = [];
+            // Map triggers to host groups
+            $group_triggers = [];
             foreach ($triggers as $trigger) {
                 foreach ($trigger['hosts'] as $trigger_host) {
                     $hostid = $trigger_host['hostid'];
-                    if (!isset($host_triggers[$hostid])) {
-                        $host_triggers[$hostid] = [];
-                    }
-                    $host_triggers[$hostid][] = $trigger;
-                }
-            }
-
-            // Check each host's status
-            foreach ($hosts as $hostid => $host) {
-                $is_healthy = true;
-                $has_critical = false;
-                $has_warning = false;
-                $has_info = false;
-                
-                if (isset($host_triggers[$hostid])) {
-                    foreach ($host_triggers[$hostid] as $trigger) {
-                        if ($trigger['value'] == TRIGGER_VALUE_TRUE) {
-                            $is_healthy = false;
-                            
-                            switch ($trigger['priority']) {
-                                case TRIGGER_SEVERITY_DISASTER:
-                                case TRIGGER_SEVERITY_HIGH:
-                                    $has_critical = true;
-                                    $statistics['critical_alerts']++;
-                                    break;
-                                case TRIGGER_SEVERITY_AVERAGE:
-                                case TRIGGER_SEVERITY_WARNING:
-                                    $has_warning = true;
-                                    $statistics['warning_alerts']++;
-                                    break;
-                                default:
-                                    $has_info = true;
-                                    $statistics['info_alerts']++;
-                                    break;
+                    
+                    // Find which customer groups this host belongs to
+                    if (isset($hosts[$hostid])) {
+                        $host = $hosts[$hostid];
+                        foreach ($host['hostgroups'] as $hg) {
+                            $groupid = $hg['groupid'];
+                            if (isset($customer_groups[$groupid])) {
+                                if (!isset($group_triggers[$groupid])) {
+                                    $group_triggers[$groupid] = [];
+                                }
+                                $group_triggers[$groupid][] = $trigger;
                             }
                         }
                     }
                 }
-
-                $host_status[$hostid] = [
-                    'host' => $host['host'],
-                    'name' => $host['name'],
-                    'healthy' => $is_healthy,
-                    'has_critical' => $has_critical,
-                    'has_warning' => $has_warning,
-                    'has_info' => $has_info,
-                    'groups' => $host['hostgroups'] ?? []
-                ];
-
-                if ($is_healthy) {
-                    $statistics['healthy_hosts']++;
-                } else {
-                    $statistics['hosts_with_alerts']++;
-                }
             }
 
-            // Group hosts by host group
-            $grouped_hosts = [];
-            foreach ($hosts as $hostid => $host) {
-                if (isset($host['hostgroups']) && is_array($host['hostgroups'])) {
-                    foreach ($host['hostgroups'] as $group) {
-                        $groupid = $group['groupid'];
-                        if (!isset($grouped_hosts[$groupid])) {
-                            $grouped_hosts[$groupid] = [
-                                'id' => $groupid,
-                                'name' => $host_groups[$groupid]['name'] ?? 'Unknown',
-                                'hosts' => []
-                            ];
+            // 4. Build group data with alert details
+            $groups_data = [];
+            $statistics = [
+                'total_groups' => count($customer_groups),
+                'healthy_groups' => 0,
+                'groups_with_alerts' => 0,
+                'total_alerts' => 0,
+                'critical_alerts' => 0,
+                'high_alerts' => 0,
+                'average_alerts' => 0,
+                'warning_alerts' => 0,
+                'info_alerts' => 0
+            ];
+
+            foreach ($customer_groups as $groupid => $group) {
+                $alert_count = 0;
+                $highest_severity = 0;
+                $severity_counts = [
+                    TRIGGER_SEVERITY_DISASTER => 0,
+                    TRIGGER_SEVERITY_HIGH => 0,
+                    TRIGGER_SEVERITY_AVERAGE => 0,
+                    TRIGGER_SEVERITY_WARNING => 0,
+                    TRIGGER_SEVERITY_INFORMATION => 0,
+                    TRIGGER_SEVERITY_NOT_CLASSIFIED => 0
+                ];
+                $alert_details = [];
+
+                if (isset($group_triggers[$groupid])) {
+                    $seen_triggers = [];
+                    foreach ($group_triggers[$groupid] as $trigger) {
+                        // Avoid counting same trigger multiple times
+                        if (isset($seen_triggers[$trigger['triggerid']])) {
+                            continue;
                         }
-                        $grouped_hosts[$groupid]['hosts'][$hostid] = $host_status[$hostid];
+                        $seen_triggers[$trigger['triggerid']] = true;
+
+                        $alert_count++;
+                        $priority = (int)$trigger['priority'];
+                        
+                        if ($priority > $highest_severity) {
+                            $highest_severity = $priority;
+                        }
+                        
+                        $severity_counts[$priority]++;
+                        
+                        // Store alert details for tooltip
+                        $alert_details[] = [
+                            'description' => $trigger['description'],
+                            'priority' => $priority,
+                            'priority_name' => $this->getSeverityName($priority)
+                        ];
+
+                        // Update statistics
+                        switch ($priority) {
+                            case TRIGGER_SEVERITY_DISASTER:
+                                $statistics['critical_alerts']++;
+                                break;
+                            case TRIGGER_SEVERITY_HIGH:
+                                $statistics['high_alerts']++;
+                                break;
+                            case TRIGGER_SEVERITY_AVERAGE:
+                                $statistics['average_alerts']++;
+                                break;
+                            case TRIGGER_SEVERITY_WARNING:
+                                $statistics['warning_alerts']++;
+                                break;
+                            default:
+                                $statistics['info_alerts']++;
+                                break;
+                        }
                     }
                 }
+
+                $is_healthy = $alert_count === 0;
+                
+                if ($is_healthy) {
+                    $statistics['healthy_groups']++;
+                } else {
+                    $statistics['groups_with_alerts']++;
+                    $statistics['total_alerts'] += $alert_count;
+                }
+
+                $groups_data[] = [
+                    'groupid' => $groupid,
+                    'name' => $group['name'],
+                    'short_name' => str_replace('CUSTOMER/', '', $group['name']),
+                    'alert_count' => $alert_count,
+                    'is_healthy' => $is_healthy,
+                    'highest_severity' => $highest_severity,
+                    'severity_counts' => $severity_counts,
+                    'alert_details' => $alert_details
+                ];
             }
 
-            // Sort groups by name
-            usort($grouped_hosts, function($a, $b) {
+            // Apply filters
+            if ($filter_alerts) {
+                $groups_data = array_filter($groups_data, function($group) {
+                    return !$group['is_healthy'];
+                });
+            }
+
+            if (!empty($search)) {
+                $search_lower = mb_strtolower($search);
+                $groups_data = array_filter($groups_data, function($group) use ($search_lower) {
+                    return strpos(mb_strtolower($group['name']), $search_lower) !== false;
+                });
+            }
+
+            // Sort by alert count (descending) then by name
+            usort($groups_data, function($a, $b) {
+                if ($a['alert_count'] != $b['alert_count']) {
+                    return $b['alert_count'] - $a['alert_count'];
+                }
                 return strcmp($a['name'], $b['name']);
             });
 
             // Calculate health percentage
-            $statistics['health_percentage'] = $statistics['total_hosts'] > 0 
-                ? round(($statistics['healthy_hosts'] / $statistics['total_hosts']) * 100, 2)
+            $statistics['health_percentage'] = $statistics['total_groups'] > 0 
+                ? round(($statistics['healthy_groups'] / $statistics['total_groups']) * 100, 1)
                 : 0;
 
             // Prepare response data
             $data = [
                 'statistics' => $statistics,
-                'groups' => $grouped_hosts,
-                'total_groups_count' => count($grouped_hosts),
+                'groups' => $groups_data,
                 'icon_size' => $icon_size,
                 'spacing' => $spacing,
+                'filter_alerts' => $filter_alerts,
+                'search' => $search,
                 'refresh' => $refresh,
                 'error' => null
             ];
@@ -186,21 +241,23 @@ class StatusPageView extends CController {
             $data = [
                 'statistics' => [
                     'total_groups' => 0,
-                    'total_hosts' => 0,
-                    'healthy_hosts' => 0,
-                    'hosts_with_alerts' => 0,
+                    'healthy_groups' => 0,
+                    'groups_with_alerts' => 0,
+                    'total_alerts' => 0,
                     'critical_alerts' => 0,
+                    'high_alerts' => 0,
+                    'average_alerts' => 0,
                     'warning_alerts' => 0,
                     'info_alerts' => 0,
                     'health_percentage' => 0
                 ],
                 'groups' => [],
-                'total_groups_count' => 0,
                 'icon_size' => $icon_size,
                 'spacing' => $spacing,
+                'filter_alerts' => $filter_alerts,
+                'search' => $search,
                 'refresh' => $refresh,
-                'error' => $e->getMessage(),
-                'test_mode' => false
+                'error' => $e->getMessage()
             ];
             
             CMessageHelper::setErrorTitle(_('Failed to fetch data: ') . $e->getMessage());
@@ -209,5 +266,17 @@ class StatusPageView extends CController {
         $response = new CControllerResponseData($data);
         $response->setTitle(_('Status Page'));
         $this->setResponse($response);
+    }
+
+    private function getSeverityName($severity) {
+        $names = [
+            TRIGGER_SEVERITY_NOT_CLASSIFIED => _('Not classified'),
+            TRIGGER_SEVERITY_INFORMATION => _('Information'),
+            TRIGGER_SEVERITY_WARNING => _('Warning'),
+            TRIGGER_SEVERITY_AVERAGE => _('Average'),
+            TRIGGER_SEVERITY_HIGH => _('High'),
+            TRIGGER_SEVERITY_DISASTER => _('Disaster')
+        ];
+        return $names[$severity] ?? _('Unknown');
     }
 }
