ef4f5e9b67cf08848cdfaab4bc234d408d9d00ce
Saumyajit <saumyajit@users.noreply.github.com>
2026-02-05 20:00:57 +0530

    Update StatusPageView.php

--- actions/StatusPageView.php
+++ actions/StatusPageView.php
@@ -3,6 +3,8 @@ namespace Modules\StatusPage\Actions;
 
 use CController;
 use CRoleHelper;
+use CMessageHelper;
+use API;
 
 class StatusPageView extends CController {
 
@@ -10,11 +12,34 @@ class StatusPageView extends CController {
         $this->disableSIDvalidation();
     }
 
+    protected function checkInput(): bool {
+        // Check if user has any preferences to save
+        $fields = [
+            'icon_size' => 'in tiny,small,medium,large',
+            'spacing' => 'in normal,compact,ultra-compact',
+            'refresh' => 'in 0,1',
+            'page' => 'ge 1'
+        ];
+
+        $ret = $this->validateInput($fields);
+
+        if (!$ret) {
+            $this->setResponse(new CControllerResponseFatal());
+        }
+
+        return $ret;
+    }
+
     protected function checkPermissions(): bool {
         return $this->checkAccess(CRoleHelper::UI_REPORTS_SYSTEM_INFO);
     }
 
     protected function doAction(): void {
+        // Get user preferences
+        $icon_size = $this->getInput('icon_size', 'small');
+        $spacing = $this->getInput('spacing', 'normal');
+        $refresh = $this->hasInput('refresh') ? (bool)$this->getInput('refresh') : false;
+
         // Fetch data from Zabbix API
         $host_groups = API::HostGroup()->get([
             'output' => ['groupid', 'name'],
@@ -22,8 +47,9 @@ class StatusPageView extends CController {
         ]);
 
         $hosts = API::Host()->get([
-            'output' => ['hostid', 'host', 'status'],
+            'output' => ['hostid', 'host', 'name', 'status'],
             'selectGroups' => ['groupid'],
+            'selectInterfaces' => ['ip', 'dns'],
             'monitored_hosts' => true,
             'preservekeys' => true
         ]);
@@ -42,7 +68,8 @@ class StatusPageView extends CController {
             'healthy_hosts' => 0,
             'hosts_with_alerts' => 0,
             'critical_alerts' => 0,
-            'warning_alerts' => 0
+            'warning_alerts' => 0,
+            'info_alerts' => 0
         ];
 
         // Process host status
@@ -51,6 +78,7 @@ class StatusPageView extends CController {
             $is_healthy = true;
             $has_critical = false;
             $has_warning = false;
+            $has_info = false;
 
             // Check triggers for this host
             foreach ($triggers as $trigger) {
@@ -58,12 +86,22 @@ class StatusPageView extends CController {
                     if ($trigger_host['hostid'] == $hostid) {
                         if ($trigger['value'] == TRIGGER_VALUE_TRUE) {
                             $is_healthy = false;
-                            if ($trigger['priority'] >= TRIGGER_SEVERITY_HIGH) {
-                                $has_critical = true;
-                                $statistics['critical_alerts']++;
-                            } else {
-                                $has_warning = true;
-                                $statistics['warning_alerts']++;
+                            
+                            switch ($trigger['priority']) {
+                                case TRIGGER_SEVERITY_DISASTER:
+                                case TRIGGER_SEVERITY_HIGH:
+                                    $has_critical = true;
+                                    $statistics['critical_alerts']++;
+                                    break;
+                                case TRIGGER_SEVERITY_AVERAGE:
+                                case TRIGGER_SEVERITY_WARNING:
+                                    $has_warning = true;
+                                    $statistics['warning_alerts']++;
+                                    break;
+                                default:
+                                    $has_info = true;
+                                    $statistics['info_alerts']++;
+                                    break;
                             }
                         }
                     }
@@ -71,9 +109,13 @@ class StatusPageView extends CController {
             }
 
             $host_status[$hostid] = [
+                'host' => $host['host'],
+                'name' => $host['name'],
                 'healthy' => $is_healthy,
                 'has_critical' => $has_critical,
-                'has_warning' => $has_warning
+                'has_warning' => $has_warning,
+                'has_info' => $has_info,
+                'interfaces' => $host['interfaces'] ?? []
             ];
 
             if ($is_healthy) {
@@ -90,6 +132,7 @@ class StatusPageView extends CController {
                 $groupid = $group['groupid'];
                 if (!isset($grouped_hosts[$groupid])) {
                     $grouped_hosts[$groupid] = [
+                        'id' => $groupid,
                         'name' => $host_groups[$groupid]['name'],
                         'hosts' => []
                     ];
@@ -98,14 +141,32 @@ class StatusPageView extends CController {
             }
         }
 
+        // Sort groups by name
+        usort($grouped_hosts, function($a, $b) {
+            return strcmp($a['name'], $b['name']);
+        });
+
+        // Calculate overall health percentage
+        if ($statistics['total_hosts'] > 0) {
+            $statistics['health_percentage'] = round(($statistics['healthy_hosts'] / $statistics['total_hosts']) * 100, 2);
+        } else {
+            $statistics['health_percentage'] = 0;
+        }
+
         // Prepare response
         $data = [
             'statistics' => $statistics,
             'groups' => $grouped_hosts,
             'total_groups_count' => count($grouped_hosts),
-            'icon_size' => 'small', // Default icon size
-            'spacing' => 'normal' // Default spacing
+            'icon_size' => $icon_size,
+            'spacing' => $spacing,
+            'refresh' => $refresh
         ];
+
+        // Add success message if refreshed
+        if ($refresh) {
+            CMessageHelper::setSuccessTitle(_('Status page refreshed successfully'));
+        }
 
         $response = new CControllerResponseData($data);
         $response->setTitle(_('Status Page'));
